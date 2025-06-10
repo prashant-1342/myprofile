74efd91efe126eb323c74b3a9340220a73518b77
Prashant Pal <prashantpal2024@gmail.com>
2025-06-10 18:33:30 +0530

    Initial commit with updated local project

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Page2 = () => {
-  const [grayscale, setgrayscale] = useState(1)
-  const [grayscale2, setgrayscale2] = useState(1)
- 
+  const [grayscale, setgrayscale] = useState(1);
+  const [grayscale2, setgrayscale2] = useState(1);
+
   return (
     <div className="page2">
       <div className="page2title">Featured Projects</div>
@@ -12,18 +11,18 @@ const Page2 = () => {
         <a target='_blank' href='https://flixdeck.vercel.app' className="page2col">
           <div onMouseOver={() => { setgrayscale(0) }} className="ps1" onMouseOut={() => { setgrayscale(1) }}>
             <figure className='blackwhite'>
-              <img className='thumbnail' src='Screenshot 2025-06-10 134729.png' />
+              <img className='thumbnail' src={`${import.meta.env.BASE_URL}Screenshot 2025-06-10 134729.png`} />
             </figure>
           </div>
           <div className="ps2">
             <div className="ps3">FlixDeck</div>
             <div>Movie exploration app</div>
           </div>
-        </a >
-        <a target='_blank' href='https://pokedex-sigma-azure.vercel.app/'className="page2col">
-        <div onMouseOver={() => { setgrayscale2(0) }} className="ps1" onMouseOut={() => { setgrayscale2(1) }}>
-            <figure  className='blackwhite'>
-              <img className='thumbnail' src='Untitled design (1).png' />
+        </a>
+        <a target='_blank' href='https://pokedex-sigma-azure.vercel.app/' className="page2col">
+          <div onMouseOver={() => { setgrayscale2(0) }} className="ps1" onMouseOut={() => { setgrayscale2(1) }}>
+            <figure className='blackwhite'>
+              <img className='thumbnail' src={`${import.meta.env.BASE_URL}Untitled design (1).png`} />
             </figure>
           </div>
           <div className="ps2">
@@ -31,13 +30,12 @@ const Page2 = () => {
             <div>Pokemon browsing app</div>
           </div>
         </a>
-
       </div>
       {/* <div className="page2row">
         <div className="page2col">
-        <div onMouseOver={() => { setgrayscale3(0) }} className="ps1" onMouseOut={() => { setgrayscale3(1) }}>
+          <div onMouseOver={() => { setgrayscale3(0) }} className="ps1" onMouseOut={() => { setgrayscale3(1) }}>
             <figure style={{ filter: grayscale3 ? "grayscale(1)" : "grayscale(0)" }} className='blackwhite'>
-              <img className='thumbnail' src='fotor-20250310224315.png' />
+              <img className='thumbnail' src={`${import.meta.env.BASE_URL}fotor-20250310224315.png`} />
             </figure>
           </div>
           <div className="ps2">
@@ -46,9 +44,9 @@ const Page2 = () => {
           </div>
         </div>
         <div className="page2col">
-        <div onMouseOver={() => { setgrayscale4(0) }} className="ps1" onMouseOut={() => { setgrayscale4(1) }}>
+          <div onMouseOver={() => { setgrayscale4(0) }} className="ps1" onMouseOut={() => { setgrayscale4(1) }}>
             <figure style={{ filter: grayscale4 ? "grayscale(1)" : "grayscale(0)" }} className='blackwhite'>
-              <img className='thumbnail' src='fotor-20250310224315.png' />
+              <img className='thumbnail' src={`${import.meta.env.BASE_URL}fotor-20250310224315.png`} />
             </figure>
           </div>
           <div className="ps2">
@@ -56,10 +54,9 @@ const Page2 = () => {
             <div>Movie browsing app</div>
           </div>
         </div>
-
       </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Page2
+export default Page2;
--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Page3 = () => {
-  const [visible1, setvisible1] = useState(false)
-  const [visible2, setvisible2] = useState(false)
-  const [visible3, setvisible3] = useState(false)
-  const [visible4, setvisible4] = useState(false)
-  const [visible5, setvisible5] = useState(false)
-  const [visible6, setvisible6] = useState(false)
-  const [visible7, setvisible7] = useState(false)
-  const [visible8, setvisible8] = useState(false)
-  const [visible9, setvisible9] = useState(false)
-  const [visible10, setvisible10] = useState(false)
-  const [visible11, setvisible11] = useState(false)
+  const [visible1, setvisible1] = useState(false);
+  const [visible2, setvisible2] = useState(false);
+  const [visible3, setvisible3] = useState(false);
+  const [visible4, setvisible4] = useState(false);
+  const [visible5, setvisible5] = useState(false);
+  const [visible6, setvisible6] = useState(false);
+  const [visible7, setvisible7] = useState(false);
+  const [visible8, setvisible8] = useState(false);
+  const [visible9, setvisible9] = useState(false);
+  const [visible10, setvisible10] = useState(false);
+  const [visible11, setvisible11] = useState(false);
+
   return (
     <div className="page3">
       <div className="page3title">
@@ -26,27 +26,23 @@ const Page3 = () => {
             </div>
             <div>
               <div className="links">
-                <a target='_blank'  href='https://github.com/prashant-1342' className="iconcontainer">
-                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"><path d="M12 4c1.67 0 2.61.4 3 .5.53-.43 1.94-1.5 3.5-1.5.34 1 .29 2.22 0 3 .75 1 1 2 1 3.5 0 2.19-.48 3.58-1.5 4.5-1.02.92-2.11 1.37-3.5 1.5.65.54.5 1.87.5 2.5v3M12 4c-1.67 0-2.61.4-3 .5C8.47 4.07 7.06 3 5.5 3c-.34 1-.29 2.22 0 3-.75 1-1 2-1 3.5 0 2.19.48 3.58 1.5 4.5 1.02.92 2.11 1.37 3.5 1.5-.65.54-.5 1.87-.5 2.5v3"></path><path d="M9 19c-1.41 0-2.84-.56-3.69-1.19-.84-.63-1.09-1.66-2.31-2.31"></path></g></svg>
+                <a target='_blank' href='https://github.com/prashant-1342' className="iconcontainer">
+                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round"><path d="M12 4c1.67 0 2.61.4 3 .5.53-.43 1.94-1.5 3.5-1.5.34 1 .29 2.22 0 3 .75 1 1 2 1 3.5 0 2.19-.48 3.58-1.5 4.5-1.02.92-2.11 1.37-3.5 1.5.65.54.5 1.87.5 2.5v3M12 4c-1.67 0-2.61.4-3 .5C8.47 4.07 7.06 3 5.5 3c-.34 1-.29 2.22 0 3-.75 1-1 2-1 3.5 0 2.19.48 3.58 1.5 4.5 1.02.92 2.11 1.37 3.5 1.5-.65.54-.5 1.87-.5 2.5v3"></path><path d="M9 19c-1.41 0-2.84-.56-3.69-1.19-.84-.63-1.09-1.66-2.31-2.31"></path></g></svg>
                 </a>
                 <a target='_blank' href='https://www.linkedin.com/in/prashantpal11/' className="iconcontainer">
-                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.8"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6M2 9h4v12H2z"></path><circle cx="4" cy="4" r="2"></circle></g></svg>
+                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeWidth="1.8"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6M2 9h4v12H2z"></path><circle cx="4" cy="4" r="2"></circle></g></svg>
                 </a>
                 <a href='mailto:prashantpal2024@gmail.com' target='_blank' className="iconcontainer">
-                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path stroke-width="1.5" fill="currentColor" d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm3.519 0L12 11.671 18.481 6zM20 7.329l-7.341 6.424a1 1 0 0 1-1.318 0L4 7.329V18h16z"></path></svg>
+                  <svg className='icons' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path strokeWidth="1.5" fill="currentColor" d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm3.519 0L12 11.671 18.481 6zM20 7.329l-7.341 6.424a1 1 0 0 1-1.318 0L4 7.329V18h16z"></path></svg>
                 </a>
-
               </div>
-
             </div>
           </div>
           <div className="aboutme">
             <div className="somemore">Some More</div>
             <div className='vbn'>I'm from India and I enjoy learning about the world of software development, I value the small details and I'm a big fan of React (such a versatile framework! 🙂).<br /><br />
-
               In my free time I usually consume content about technology, play video games, improve my creativity and communication skills, solve Data Structure and algorithms problems or generate ideas for my projects.<br /><br />
               I really enjoy working on the web, making it an accessible, enjoyable and efficient place for all types of users.<br /><br />
-
               Currently, I am deepening my knowledge in the React ecosystem , but I am always willing to learn new technologies (Angular.js ).</div>
           </div>
           <div className="aboutme rfd">
@@ -54,50 +50,48 @@ const Page3 = () => {
             <div className="stacks">
               <div onMouseOver={() => setvisible1(true)} onMouseOut={() => setvisible1(false)} className='stackname'>
                 <div style={{ visibility: visible1 ? "visible" : "hidden" }} className="notify">React</div>
-                <img className='stackicon' src='physics.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}physics.png`} />
               </div>
               <div onMouseOver={() => setvisible2(true)} onMouseOut={() => setvisible2(false)} className='stackname'>
                 <div style={{ visibility: visible2 ? "visible" : "hidden" }} className="notify">Next.js</div>
-                <img className='stackicon' src='icons8-nextjs-480.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-nextjs-480.png`} />
               </div>
-
               <div onMouseOver={() => setvisible3(true)} onMouseOut={() => setvisible3(false)} className='stackname'>
                 <div style={{ visibility: visible3 ? "visible" : "hidden" }} className="notify">Typescript</div>
-                <img className='stackicon' src='icons8-typescript-480.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-typescript-480.png`} />
               </div>
-
               <div onMouseOver={() => setvisible4(true)} onMouseOut={() => setvisible4(false)} className='stackname'>
                 <div style={{ visibility: visible4 ? "visible" : "hidden" }} className="notify">Node.js</div>
+                {/* External image, leave as is */}
                 <img className='stackicon' src='https://cdn.freebiesupply.com/logos/large/2x/nodejs-icon-logo-png-transparent.png' />
               </div>
-
               <div onMouseOver={() => setvisible5(true)} onMouseOut={() => setvisible5(false)} className='stackname'>
                 <div style={{ visibility: visible5 ? "visible" : "hidden" }} className="notify">Express.js</div>
-                <img className='stackicon ' src='icons8-express-js-500.png' />
+                <img className='stackicon ' src={`${import.meta.env.BASE_URL}icons8-express-js-500.png`} />
               </div>
               <div onMouseOver={() => setvisible6(true)} onMouseOut={() => setvisible6(false)} className='stackname'>
                 <div style={{ visibility: visible6 ? "visible" : "hidden" }} className="notify">HTML</div>
-                <img className='stackicon' src='icons8-html-480.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-html-480.png`} />
               </div>
               <div onMouseOver={() => setvisible7(true)} onMouseOut={() => setvisible7(false)} className='stackname'>
                 <div style={{ visibility: visible7 ? "visible" : "hidden" }} className="notify">CSS</div>
-                <img className='stackicon' src='icons8-css-240.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-css-240.png`} />
               </div>
               <div onMouseOver={() => setvisible8(true)} onMouseOut={() => setvisible8(false)} className='stackname'>
                 <div style={{ visibility: visible8 ? "visible" : "hidden" }} className="notify">Javascript</div>
-                <img className='stackicon' src='icons8-javascript-480.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-javascript-480.png`} />
               </div>
               <div onMouseOver={() => setvisible9(true)} onMouseOut={() => setvisible9(false)} className='stackname'>
                 <div style={{ visibility: visible9 ? "visible" : "hidden" }} className="notify">Tailwind</div>
-                <img className='stackicon' src='icons8-tailwind-css-240.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-tailwind-css-240.png`} />
               </div>
               <div onMouseOver={() => setvisible10(true)} onMouseOut={() => setvisible10(false)} className='stackname'>
                 <div style={{ visibility: visible10 ? "visible" : "hidden" }} className="notify">MongoDb</div>
-                <img className='stackicon' src='MongoDB.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}MongoDB.png`} />
               </div>
               <div onMouseOver={() => setvisible11(true)} onMouseOut={() => setvisible11(false)} className='stackname'>
                 <div style={{ visibility: visible11 ? "visible" : "hidden" }} className="notify">Git</div>
-                <img className='stackicon' src='icons8-github-240.png' />
+                <img className='stackicon' src={`${import.meta.env.BASE_URL}icons8-github-240.png`} />
               </div>
             </div>
           </div>
@@ -105,7 +99,7 @@ const Page3 = () => {
         </div>
         <div className="p3col">
           <div className="profilepicture">
-            <img className='prph' src='skin.webp' />
+            <img className='prph' src={`${import.meta.env.BASE_URL}skin.webp`} />
             <div className="info">
               <span className='infodetail'>
                 <span className='sdf'>Hello ✋</span>
@@ -125,15 +119,14 @@ const Page3 = () => {
               <div className="exp11">2024 Present</div>
               <div className='exp12'>
                 <div className="expname"> Web Developer</div>
-                <a target='_blank' href='https://www.linkedin.com/company/dsc-dtu/' className='r1'>GDG DTU <img className='toicon' src='up-right-arrow.png' /></a>
-
+                <a target='_blank' href='https://www.linkedin.com/company/dsc-dtu/' className='r1'>GDG DTU <img className='toicon' src={`${import.meta.env.BASE_URL}up-right-arrow.png`} /></a>
               </div>
             </div>
             <div className="exp1">
               <div className="exp11">2023 — 2024</div>
               <div className="exp12">
                 <div className="expname"> Frontend Developer</div>
-                <a target='_blank' href='https://dtu-connect.netlify.app/' className='r1'  >DTU Connect <img className='toicon' src='up-right-arrow.png' /></a>
+                <a target='_blank' href='https://dtu-connect.netlify.app/' className='r1'  >DTU Connect <img className='toicon' src={`${import.meta.env.BASE_URL}up-right-arrow.png`} /></a>
               </div>
             </div>
           </div>
@@ -153,12 +146,7 @@ const Page3 = () => {
               <div className="exp12">CLASS 10th - 86.4%</div>
             </div>
           </div>
-
-
-
         </div>
-
-
       </div>
     </div>
   )
